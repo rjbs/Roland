a39b4fbac3b769a4c24ecb1e4402166047a5dbf1
Ricardo Signes <rjbs@cpan.org>
2012-11-30 21:17:07 -0500

    a result class for "something went wrong"

---
+++ lib/Roland/Result/Error.pm
@@ -0,0 +1,24 @@
+package Roland::Result::Error;
+use Moose;
+with 'Roland::Result';
+
+use namespace::autoclean;
+
+has resource => (is => 'ro', isa => 'Str', required => 1);
+has error    => (is => 'ro', isa => 'Str', required => 1);
+
+sub as_text {
+  my ($self, $indent) = @_;
+
+  my $text = $self->error;
+
+  my $string = "error with " . $self->resource . "\n"
+             . $self->error . "\n";
+
+  $string =~ s{^}
+              {'  ' x ($indent // 0)}egm;
+
+  return $string;
+}
+
+1;
