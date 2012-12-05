28816a2048f73f9a39fdcefb4285639ea795b4f6
Ricardo Signes <rjbs@cpan.org>
2012-12-05 08:52:06 -0500

    note about Group being always random

--- lib/Roland/Table/Group.pm
+++ lib/Roland/Table/Group.pm
@@ -29,6 +29,12 @@ sub roll_table {
   my @keys = (0 .. $#list);
 
   if ($self->_guts->{pick}) {
+    # This is not determinable right now.  This should be fixed.  The question
+    # is: how?  Do we want to add a ->pick_n_randomly(@list) to Roller?  Do we
+    # want to roll dice over and over to pick?  Remember that this solution
+    # avoided any stupid "I had to roll 10,000 times" or "I couldn't pick as
+    # many as were requested," so do not introduce those bugs with the later
+    # version. -- rjbs, 2012-12-05
     my @shuffled_keys = shuffle @keys;
     splice @shuffled_keys, $self->_guts->{pick};
     @keys = sort { $a <=> $b } @shuffled_keys;
