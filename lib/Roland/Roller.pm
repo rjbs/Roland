e174f0e1414166929d47ed72983d497119a69536
Ricardo Signes <rjbs@cpan.org>
2012-12-05 23:41:36 -0500

    rollers do not need to see the hub (yet?)

--- lib/Roland/Roller.pm
+++ lib/Roland/Roller.pm
@@ -3,13 +3,6 @@ use Moose::Role;
 
 use namespace::autoclean;
 
-has hub => (
-  is  => 'ro',
-  isa => 'Object',
-  required => 1,
-  weak_ref => 1,
-);
-
 requires 'roll_dice';
 requires 'pick_n';
 
